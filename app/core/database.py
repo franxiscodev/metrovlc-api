8013954715dd7151fd2e7d304c3cdaccd739604f
FranxiscoDev <franxiscodev@gmail.com>
2025-10-12 16:24:46 +0200

    feat: configuracion inicial de sqlalchemy con el sqlite

---
+++ app/core/database.py
@@ -0,0 +1,40 @@
+"""
+Configurar la DB sqlite y sqlalchemy
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+# importar el archivo de configuracion
+from app.core.config import settings
+
+# https://docs.sqlalchemy.org/en/14/core/engines.html#sqlalchemy.create_engine
+# la opción echo sirve para cambiar entrre si queremos o no ver las queries SQL por terminal
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    echo=True
+)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+# la cnx en si misma
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Crear la DB y las tablas al inicializar la app
+# , aun en proceso -> crear los modelos
+
+
+def init_db():
+    # from app.models import tablausuarios, tablaestaciones
+    # create_all
+    # Base.metadata.create_all(bind=engine)
+    print("base creada ok!")
