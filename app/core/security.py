ede546de2a3c29caae1e514382d816b2a4c1d7f0
FranxiscoDev <franxiscodev@gmail.com>
2025-10-12 19:34:54 +0200

    feat: implementar autenticacion JWT

---
+++ app/core/security.py
@@ -0,0 +1,52 @@
+"""
+Autenticacion y Autorizacion
+para puntos extras ;)
+
+funciones de seguridad
+"""
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+# leer configuraciones privada de las variables de entorno
+from app.core.config import settings
+
+# para hashear las passwords, el algoritmo bcrypt parece ser el standadr
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# generar un hash con el algoritmo de pwd_context que se guara en la db
+def get_pwd_hash(pwd: str) -> str:
+    return pwd_context.hash(pwd)
+
+
+# verificar si el password enviado coincide con su hash
+def verify_password(plain_pwd: str, hashed_pwd: str) -> bool:
+    return pwd_context.verify(plain_pwd, hashed_pwd)
+
+
+# crear el token de acceso JWT
+def create_acces_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    # uso de las variables de entorno
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
+
+
+# decodificar y validar token
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+        return payload
+    except JWTError:
+        return None
