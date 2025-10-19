1bc147d3b26c5d26e3261b765a93b9f1112837ce
FranxiscoDev <franxiscodev@gmail.com>
2025-10-19 21:30:05 +0200

    feat: schema de stations, busquedas, llegadas,  favoritos

---
+++ app/schemas/station.py
@@ -0,0 +1,76 @@
+"""
+estructura de los datos de station
+request - response
+"""
+
+
+from pydantic import BaseModel, Field, ConfigDict
+from typing import List, Optional
+from datetime import datetime
+
+
+class StationBase(BaseModel):
+    codigo: str
+    nombre: str
+    linea: str
+
+
+class StationResponse(StationBase):
+    id: int
+    gid: int
+    nombre_normalizado: str
+    geo_point_2d: str | None = None
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class StationSimple(BaseModel):
+    """
+    para listados
+    """
+    id: int
+    codigo: str
+    nombre: str
+    linea: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+ # búsquedas
+
+
+class StationSearchResponse(BaseModel):
+    """
+    respuesta de búsqueda de estaciones
+    """
+    results: List[StationSimple]
+    total: int
+    query: str
+    links: dict
+
+
+# llegadas de coches
+class ArrivalInfo(BaseModel):
+    linea: str | None = None
+    destino: str | None = None
+    tiempo: str | None = None
+    tipo: str | None = None
+
+
+# Fsavoritos
+class AddFavoriteResponse(BaseModel):
+    """
+    respuesta al añadir estación a favoritos
+    """
+    message: str
+    station: StationSimple
+    total_favorites: int
+
+
+class FavoriteStationsResponse(BaseModel):
+    """
+    listar estaciones favoritas de un usuario
+    """
+    favorites: List[StationSimple]
+    total: int
+    links: dict
